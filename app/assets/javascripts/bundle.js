2199f466c69e053665d545befef804c71b0cdfdc
Christine Liao <astridcliao@gmail.com>
2016-01-03 15:03:47 -0800

    upload button wave effect added

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -31783,10 +31783,6 @@
 	    cloudinary.openUploadWidget(CLOUDINARY_OPTIONS, (function (error, results) {
 	      if (!error) {
 	        var image = results[0];
-	        //here we probably want to include public id at some point
-	        //for now we will just use url
-	        //we will eventually probably store public id instead of url
-	        // debugger below was addImage(image.url)
 	        this.props.addImage(image.public_id);
 	      }
 	    }).bind(this));
@@ -31798,7 +31794,7 @@
 	      null,
 	      React.createElement(
 	        "button",
-	        { className: "btn btn-minor", onClick: this.upload },
+	        { className: "btn btn-minor waves-effect waves-light", onClick: this.upload },
 	        "Upload dish image!"
 	      )
 	    );
--- frontend/components/UploadButton.jsx
+++ frontend/components/UploadButton.jsx
@@ -6,10 +6,6 @@ var UploadButton = React.createClass({
     cloudinary.openUploadWidget(CLOUDINARY_OPTIONS, function (error, results) {
       if(!error) {
         var image = results[0];
-        //here we probably want to include public id at some point
-        //for now we will just use url
-        //we will eventually probably store public id instead of url
-        // debugger below was addImage(image.url)
         this.props.addImage(image.public_id);
       }
     }.bind(this));
@@ -19,7 +15,7 @@ var UploadButton = React.createClass({
   render: function () {
     return (
       <div>
-        <button className="btn btn-minor" onClick={this.upload}>Upload dish image!</button>
+        <button className="btn btn-minor waves-effect waves-light" onClick={this.upload}>Upload dish image!</button>
       </div>
     );
   }
